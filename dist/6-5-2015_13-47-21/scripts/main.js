c6fb26dabba77a24962988dd0d4b5a55f1cb6c94
abberg <andrewbberg@gmail.com>
2015-06-05 13:47:21 -0400

    new public version

---
+++ dist/6-5-2015_13-47-21/scripts/main.js
@@ -0,0 +1,37 @@
+(function(){
+
+	"use strict";
+	
+	var loop = ab.gameLoop(),
+		three = ab.threeBase(),
+		sketch = ab.sketch(three);
+
+	loop.addEventListener('framestart', function(event){
+
+		var timestamp = event.detail.timestamp;
+		sketch.framestart(timestamp);
+	
+	})
+
+	loop.addEventListener('frameupdate', function(event){
+
+		var timestep = event.detail.timestep;
+		sketch.update(timestep);
+	
+	})
+
+	loop.addEventListener('framedraw', function(event){
+
+		var interpolation = event.detail.interpolation;
+		sketch.draw(interpolation);
+
+	});
+
+	sketch.init();
+	loop.start();
+
+	if(ab.controlBar){
+		ab.controlBar(loop, three);
+	}
+
+}())
